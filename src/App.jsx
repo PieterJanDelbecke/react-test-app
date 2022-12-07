370b99c97c30ee0c58b0afe9db691012351c266d
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-12-08 08:41:57 +1100

    dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
 
 function App() {
+  const testFieldArray = [
+    { id: "001", name: "Status" },
+    { id: "002", name: "numbers" },
+  ];
+
   return (
     <>
       <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
+      <select onChange={(e) => setSelectedFieldNumber(e.target.value)}>
+        {testFieldArray.map((field, index) => (
+          <option key={field.id} value={index}>
+            {field.name}
+          </option>
+        ))}
+      </select>
     </>
   );
 }
