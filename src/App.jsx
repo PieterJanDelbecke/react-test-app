a3c49cccab1bfa81359bc242667b967627ca9a34
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-12-08 15:49:01 +1100

    setup hompage

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./components/home";
 
 function App() {
-  const testFieldArray = [
-    { id: "001", name: "Status" },
-    { id: "002", name: "numbers" },
-  ];
-
   return (
     <>
-      <h1>Hello World</h1>
-      <select onChange={(e) => setSelectedFieldNumber(e.target.value)}>
-        {testFieldArray.map((field, index) => (
-          <option key={field.id} value={index}>
-            {field.name}
-          </option>
-        ))}
-      </select>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
