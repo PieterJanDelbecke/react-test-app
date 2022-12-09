3c37ec2cbb1887b91163b4f23f63d16c11547d56
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-12-09 15:19:46 +1100

    WIP: initail stage of dropdown

--- src/components/dropdown.jsx
+++ src/components/dropdown.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import styled from "@emotion/styled";
 
 const DropdownForm = styled.form`
@@ -16,58 +16,86 @@ const DropdownButton = styled.button`
 const Content = styled.div`
   background-color: yellow;
   width: 200px;
-  height: 300px;
+  /* height: 300px; */
 `;
 
 const Option = styled.div`
+  position: relative;
   height: 24px;
   border: 1px solid red;
 `;
 const Input = styled.input`
-  /* display: none; */
+  /* visibility: hidden; */
+  /* height: 0;
+  width: 0; */
+  opacity: 0;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 24px;
+  z-index: 1;
+`;
+
+const Label = styled.label`
+  position: absolute;
+  left: 10px;
+  z-index: 0;
 `;
 
 function Dropdown({}) {
-  const handleClickFilm = () => {
-    console.log("Film");
-  };
+  const options = [
+    { id: "001", label: "one" },
+    { id: "002", label: "two" },
+    { id: "003", label: "three" },
+    { id: "004", label: "four" },
+    { id: "005", label: "five" },
+  ];
+  const inputRef = useRef(null);
+  const [open, setOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(options[0].label);
 
-  const handleClickMusic = () => {
-    console.log("Music");
-  };
-  const handleClickArt = () => {
-    console.log("Art");
-  };
+  useEffect(() => {
+    inputRef.current.value = options[0].label;
+  }, []);
 
   const save = (e) => {
+    e.preventDefault();
     console.log("SUBMIT");
     const formData = new FormData(e.target);
     console.log([...formData.entries()]);
   };
 
+  const handleClick = (e) => {
+    console.log("CLICKED");
+    setSelectedOption(e.target.value);
+    inputRef.current.value = e.target.value;
+    console.log(e.target.value);
+  };
+
+  console.log("OPEN:", open);
+
   return (
     <DropdownForm onSubmit={save}>
       <DropdownButton type="button" onClick={() => setOpen((prev) => !prev)}>
-        Click
+        {selectedOption}
       </DropdownButton>
-      <Content
-        onChange={(e) => {
-          console.log(e.target.value);
-        }}
-      >
-        <Option>
-          <input type="radio" value="one" name="numbers" />
-          <label htmlFor="numbers">One</label>
-        </Option>
-        <Option>
-          <input type="radio" value="two" name="numbers" />
-          <label htmlFor="numbers">two</label>
-        </Option>
-        <Option>
-          <input type="radio" value="three" name="numbers" />
-          <label htmlFor="numbers">three</label>
-        </Option>
+      {/* {open && ( */}
+      <Content>
+        {options.map((option) => (
+          <Option key={option.id} onClick={handleClick}>
+            <Input
+              ref={inputRef}
+              type="radio"
+              id={option.id}
+              value={option.label}
+              name="numbers"
+            />
+            <Label htmlFor="numbers">{option.label}</Label>
+          </Option>
+        ))}
       </Content>
+      {/* )} */}
       <button>SUBMIT</button>
     </DropdownForm>
   );
---
+++ src/components/notepad.jsx
@@ -0,0 +1,10 @@
+{
+  /* <label htmlFor="inputForm1">Input1</label>
+      <input type="text" name="inputForm1" id="001" />
+      <label htmlFor="inputForm">Input2</label>
+      <input type="text" name="inputForm2" id="002" />
+      <label htmlFor="inputForm">Input3</label>
+      <input type="text" name="inputForm3" id="003" />
+      <label htmlFor="inputForm">Input4</label>
+      <input type="text" name="inputForm4" id="004" /> */
+}
